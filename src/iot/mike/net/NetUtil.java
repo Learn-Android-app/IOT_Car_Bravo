4bd23c986597fa20a1258e7bb2a16845ebe44580
MikeCoder <mikecoder13@gmail.com>
2013-08-05 23:21:38 +0800

    Test

--- src/iot/mike/net/NetUtil.java
+++ src/iot/mike/net/NetUtil.java
@@ -1,9 +1,11 @@
 package iot.mike.net;
 
+import iot.mike.data.ResultType;
+
 public class NetUtil {
 	private NetUtil(){}
 	
 	public static int confirmType(String jsonorder){
-		
+		return ResultType.Result_GPS;
 	}
 }
